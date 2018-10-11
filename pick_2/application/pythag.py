9063598babe2f3e9aa599b5f0095bedccbaae519
Ismael Leyva <2020ileyva@01889.dwight.edu>
2018-10-11 13:24:17 -0400

    init

---
+++ pick_2/application/pythag.py
@@ -0,0 +1,28 @@
+# You need to design & create a function to make a client's life better.
+
+## Step Zero:
+### Find your Client and their needs
+# my client
+## Step One: Gantt Chart and Concurrent Programming
+### In the form of a Gantt chart, fully plan out the tasks and time needed to complete this task.
+
+## Step Two: Creation
+### Create the function!
+
+## Step Three: Beta Testing
+### What can you change and how?
+
+## Step Four: User Documentation
+### Create user documentation for your client. What should it contain?
+
+#How to check by using the Pythagoream theorm: a*a + b*b = c*c
+
+
+def check(a,b,c):
+    if a*a + b*b == c*c:
+        print(c*c)
+    elif a*a + b*b > c*c:
+        print('does not work')
+    elif a*a + b*b < c*c:
+        print('does not work')
+check(4,6,5)
---
+++ pick_2/obi_wan/work.py
@@ -0,0 +1,12 @@
+# Teach the Padawans (middle schoolers) the ways of the force.
+
+## Your goal is to create a lesson to teach absolute beginners how to program! There are two sections you may pick from. Your students must walk away with a working program and a class average of a B on your self made assessment.
+
+## Section 1
+### Criteria to Cover in your lesson
+- variables
+- arrays
+- if/else
+- for loops
+
+padawans/middleschoolers == x
