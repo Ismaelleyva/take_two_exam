856dca19612925b6105cfb06d2aea03af9417309
Ismael Leyva <2020ileyva@01889.dwight.edu>
2018-10-12 12:22:20 -0400

    values of a*a + b*b

--- pick_2/application/pythag.py
+++ pick_2/application/pythag.py
@@ -2,27 +2,25 @@
 
 ## Step Zero:
 ### Find your Client and their needs
-# my client
+#  my client needs a program to check the pythagoream theorm so he can spend more time studding than checking answeres. This program can do just that for him.
+
 ## Step One: Gantt Chart and Concurrent Programming
 ### In the form of a Gantt chart, fully plan out the tasks and time needed to complete this task.
 
 ## Step Two: Creation
 ### Create the function!
-
-## Step Three: Beta Testing
-### What can you change and how?
-
-## Step Four: User Documentation
-### Create user documentation for your client. What should it contain?
-
-#How to check by using the Pythagoream theorm: a*a + b*b = c*c
-
-
 def check(a,b,c):
     if a*a + b*b == c*c:
-        print(c*c)
+        print(c*c, 'right triangle')
     elif a*a + b*b > c*c:
-        print('does not work')
+        print('acute triangle')
     elif a*a + b*b < c*c:
-        print('does not work')
-check(4,6,5)
+        print('obtuse triangle')
+
+check(3,4,5)
+## Step Three: Beta Testing
+### What can you change and how?
+#I can change the fact that this program is not able to use the square root of numbers.
+## Step Four: User Documentation
+### Create user documentation for your client. What should it contain?
+#This program can only be used to check if you have the answeres for a,b, and c. this program is just used to make your homework a little easier.
